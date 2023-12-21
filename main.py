a3c75d77a8ab18bd7814e9ea74bda4bf5f2b9334
Vinícius <vinicius.solique@estudante.ifms.edu.br>
2023-12-21 11:30:41 -0400

    Listagem de números de 1 a 9

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-notas = []
-
-for x in range(2):
-  codigo_aluno = input("RM: ")
-  nota = float(input("Nota: "))
-  resultado = [codigo_aluno, nota]
-  notas.append(resultado)
-
-  print ("quantidade de notas", len(notas))
-
-  for n in notas:
-    codigo_aluno = n[0]
-    nota = n[1]
-    print("O RM", codigo_aluno, "tirou nota:", nota)
+for x in range(10):
+    print(x)
