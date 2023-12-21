2b8b027f8097d21d95fd1a532ed825b4baf8fbeb
Vinícius <vinicius.solique@estudante.ifms.edu.br>
2023-12-21 11:32:24 -0400

    Lista de Números

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numeros = [10, 9, 8, 7, 6]
+lista_numeros = [1, 2, 3]
 
-print("total", len(numeros) )
-print ("menor valor:", min(numeros) )
-print("maior valor", max(numeros) )
+lista_numeros[0] = 5
+
+print(lista_numeros[0])
+print(lista_numeros[1])
+print(lista_numeros[2])
