bbb3678370936e9f5a2b38f9b0496460fc08061e
Vinícius <vinicius.solique@estudante.ifms.edu.br>
2023-12-21 11:28:45 -0400

    Lista de Notas

--- main.py
+++ main.py
@@ -13,4 +13,7 @@ while contador <= 2:
 
   print ("quantidade de notas", len(notas))
 
- 
+  for n in notas:
+    codigo_aluno = n[0]
+    nota = n[1]
+    print("O RM", codigo_aluno, "tirou nota:", nota)
