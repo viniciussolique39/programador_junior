91d724b91e5904f78eba41dd8dca97a37f126721
Vinícius <vinicius.solique@estudante.ifms.edu.br>
2023-12-21 11:29:31 -0400

    for x in range

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 notas = []
 
-contador = 1 
-
-
-while contador <= 2:
+for x in range(2):
   codigo_aluno = input("RM: ")
   nota = float(input("Nota: "))
   resultado = [codigo_aluno, nota]
   notas.append(resultado)
-
-  contador = contador + 1
 
   print ("quantidade de notas", len(notas))
 
